ea24697ae7fc24ebc73f226c94b313194b455e16
dewiwulandari <wulankeren02@gmail.com>
2024-06-26 00:01:55 +0700

    update config

--- src/main/java/com/system/aplikasi/pos/MainApplication.java
+++ src/main/java/com/system/aplikasi/pos/MainApplication.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://aplikasipos-production.up.railway.app", description = "Default Server URL")})
+@OpenAPIDefinition(servers = {@Server(url = "https://aplikasipos-production-c018.up.railway.app", description = "Default Server URL")})
 @SpringBootApplication
 public class MainApplication {
 
